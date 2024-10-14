acefa8de8e1af3b887e1ddf900c9295877d1abc1
Burkay Tunçtürk <burkay.tuncturk@gmail.com>
2024-10-14 19:29:19 +0300

    ignore

---
+++ cmake-build-debug/CMakeFiles/Homework_1.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Homework_1.dir/Mission.cpp.o"
+  "CMakeFiles/Homework_1.dir/Mission.cpp.o.d"
+  "CMakeFiles/Homework_1.dir/SpaceMissionManagementSystem.cpp.o"
+  "CMakeFiles/Homework_1.dir/SpaceMissionManagementSystem.cpp.o.d"
+  "CMakeFiles/Homework_1.dir/Spacecraft.cpp.o"
+  "CMakeFiles/Homework_1.dir/Spacecraft.cpp.o.d"
+  "CMakeFiles/Homework_1.dir/main.cpp.o"
+  "CMakeFiles/Homework_1.dir/main.cpp.o.d"
+  "Homework_1"
+  "Homework_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Homework_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
