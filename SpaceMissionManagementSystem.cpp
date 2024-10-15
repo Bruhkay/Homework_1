84cdb3f25f17ee3de3c62217460b9998f1b62a14
Burkay Tunçtürk <burkay.tuncturk@gmail.com>
2024-10-15 09:54:54 +0300

    print statement mistakes fixed

--- SpaceMissionManagementSystem.cpp
+++ SpaceMissionManagementSystem.cpp
@@ -247,10 +247,12 @@ void SpaceMissionManagementSystem::showMission(const string name) const {
             cout << "  Name: "<< missions[i].getName()<< endl;
             cout << "  Launch Date: "<< missions[i].getDate()<< endl;
             cout << "  Destination: "<< missions[i].getDestination()<< endl;
-            cout << "  Assigned Spacecraft:" << endl;
+            cout << "  Assigned Spacecrafts:" << endl;
+            if(missions[i].getCount() ==0) {
+                cout << "    None" << endl;
+            }
             for(int j = 0; j<missions[i].getCount();j++) {
                 cout<<"  - "<<missions[i].getSpacecrafts(j).getName()<<endl;
-                // TODO should print none in case
             }
         }
         if(i==missionSize - 1) {
